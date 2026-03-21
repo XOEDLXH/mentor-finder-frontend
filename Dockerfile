4eeefb581179332d6de863262601af20d525e95d
syj <syj24@mails.tsinghua.edu.cn>
2026-03-21 16:17:17 +0800

    Update deploy settings

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,35 @@
-# TODO Start: [Student] Complete Dockerfile
-FROM scratch
+FROM node:22 AS builder
+
+WORKDIR /build
+
+# TODO Start: Update this with your backend URL
+ENV BACKEND_URL https://backend-mentorfinder.app.spring26a.secoder.net//:path*
 # TODO End
+
+RUN corepack enable pnpm
+
+COPY package.json pnpm-lock.yaml ./
+
+RUN pnpm config set registry https://registry.npmmirror.com
+
+RUN pnpm install
+
+COPY . .
+
+RUN pnpm build
+
+FROM node:22 AS runner
+
+WORKDIR /app
+
+COPY --from=builder /build/.next/standalone .
+
+COPY --from=builder /build/.next/static .next/static
+
+COPY --from=builder /build/public public
+
+ENV PORT 80
+
+EXPOSE 80
+
+CMD ["node", "server.js"]
--- next.config.js
+++ next.config.js
@@ -1,17 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // TODO Start: [Student] Enable standalone build
-    output: undefined,
-    // TODO End
+    output: "standalone",
     reactStrictMode: false, /* @note: To prevent duplicated call of useEffect */
     // swcMinify: true,
 
     async rewrites() {
         return [{
             source: "/api/:path*",
-            // TODO Start: [Student] Change to standard backend URL
-            destination: "http://127.0.0.1:8000/:path*",
-            // TODO End
+            destination: process.env.NODE_ENV != "production" ? "http://127.0.0.1:8000/:path*" : process.env.BACKEND_URL,
         }];
     }
 };
