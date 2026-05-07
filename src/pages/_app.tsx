911c79886a854e7eaaeed0a516fa1d1035a6338b
XOEDLXH <liamleexoed@gmail.com>
2026-05-07 23:53:57 +0800

    feat: add global top navigation shell #29

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,16 @@
 import Head from "next/head";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import store, { RootState } from "../redux/store";
-import { hydrateAuth, loadAuthFromStorage, resetAuth } from "../redux/auth";
 import { useEffect } from "react";
-import { useRouter } from "next/router";
-import { Provider, useSelector, useDispatch } from "react-redux";
+import { Provider, useDispatch } from "react-redux";
+
+import TopNav from "../components/TopNav";
+import { hydrateAuth, loadAuthFromStorage } from "../redux/auth";
+import store from "../redux/store";
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 const App = ({ Component, pageProps }: AppProps) => {
-    const router = useRouter();
     const dispatch = useDispatch();
-    const auth = useSelector((state: RootState) => state.auth);
-    const isAuthPage = router.pathname === "/login" || router.pathname === "/register";
 
     useEffect(() => {
         dispatch(hydrateAuth(loadAuthFromStorage()));
@@ -23,68 +21,11 @@ const App = ({ Component, pageProps }: AppProps) => {
             <Head>
                 <title>找导师</title>
             </Head>
-            <div className={isAuthPage ? "appShell" : "appShell appShellWithAuth"}>
-                <Component {...pageProps} />
-                {!isAuthPage && (auth.token ? (
-                    <div className="authControls">
-                        <p className="authName">用户名：{auth.name}</p>
-                        {auth.role === "admin" && (
-                            <button onClick={() => router.push("/admin-users")}>
-                                进入用户管理
-                            </button>
-                        )}
-                        <button onClick={() => dispatch(resetAuth())}>
-                            登出
-                        </button>
-                    </div>
-                ) : (
-                    <div className="authControls">
-                        <button onClick={() => router.push("/login")}>登录</button>
-                        <button onClick={() => router.push("/register")}>注册</button>
-                    </div>
-                ))}
-
-                <style jsx>{`
-                    .appShell {
-                        padding: 12px;
-                    }
-
-                    .authControls {
-                        display: flex;
-                        flex-direction: row;
-                        gap: 8px;
-                        align-items: center;
-                        flex-wrap: wrap;
-                    }
-
-                    .authName {
-                        margin: 0;
-                    }
-
-                    .appShellWithAuth {
-                        position: relative;
-                        padding-top: 92px;
-                    }
-
-                    .appShellWithAuth .authControls {
-                        position: absolute;
-                        top: 32px;
-                        right: 12px;
-                        max-width: 260px;
-                        justify-content: flex-end;
-                    }
-
-                    .appShellWithAuth .authControls button:last-child {
-                        flex-basis: 100%;
-                        margin-left: auto;
-                    }
-
-                    @media (max-width: 720px) {
-                        .appShellWithAuth {
-                            padding-top: 116px;
-                        }
-                    }
-                `}</style>
+            <div className="appChrome">
+                <TopNav />
+                <main className="appMain">
+                    <Component {...pageProps} />
+                </main>
             </div>
         </>
     );
